0cd2134a4f26ef7a071585c7cefec937a0410718
Harrison Harnisch <hharnisc@gmail.com>
2017-01-12 18:50:06 -0600

    stub in ColabTool

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+import { configure } from '@kadira/storybook';
+
+// automatically import all story.js files
+const req = require.context('../', true, /story\.js$/);
+
+function loadStories() {
+  req.keys().forEach(req);
+}
+
+configure(loadStories, module);
