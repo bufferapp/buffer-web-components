0d18efbc64388797ae4632e2f2a5a0ed69d92b7c
Harrison Harnisch <hharnisc@gmail.com>
2017-02-21 18:34:24 +0000

    Implement TextPost (#15)
    * stub in TextPost
    
    * implement TextPost

---
+++ ColabTool/TextPost/index.jsx
@@ -0,0 +1,74 @@
+import React, { PropTypes } from 'react';
+import {
+  LinkifiedText,
+} from '@bufferapp/components';
+import style from './style.css';
+import Post from '../Shared/Post';
+
+const TextPost = ({
+  isConfirmingDelete,
+  isDeleting,
+  isWorking,
+  imageSrc,
+  links,
+  manager,
+  onApproveClick,
+  onCancelConfirmClick,
+  onDeleteClick,
+  onDeleteConfirmClick,
+  onEditClick,
+  profile,
+  text,
+  retweetProfile,
+}) => {
+  const children = (
+    <div className={style['post-content']}>
+      <span className={style['post-content-text']}>
+        <LinkifiedText
+          links={links}
+        >
+          {text}
+        </LinkifiedText>
+      </span>
+    </div>
+  );
+
+  return (
+    <Post
+      isConfirmingDelete={isConfirmingDelete}
+      isDeleting={isDeleting}
+      isWorking={isWorking}
+      imageSrc={imageSrc}
+      links={links}
+      manager={manager}
+      onApproveClick={onApproveClick}
+      onCancelConfirmClick={onCancelConfirmClick}
+      onDeleteClick={onDeleteClick}
+      onDeleteConfirmClick={onDeleteConfirmClick}
+      onEditClick={onEditClick}
+      profile={profile}
+      postType={retweetProfile ? 'retweet' : 'image'}
+      text={text}
+      retweetProfile={retweetProfile}
+    >
+      {children}
+    </Post>
+  );
+};
+
+TextPost.propTypes = {
+  ...Post.commonPropTypes,
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      rawString: PropTypes.string,
+      displayString: PropTypes.string,
+      expandedUrl: PropTypes.string,
+      indices: PropTypes.arrayOf(React.PropTypes.number),
+    }),
+  ).isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+TextPost.defaultProps = Post.defaultProps;
+
+export default TextPost;
---
+++ ColabTool/TextPost/story.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  storiesOf,
+  action,
+} from '@kadira/storybook';
+import TextPost from './index';
+
+const links = [{
+  rawString: 'http://buff.ly/1LTbUqv',
+  displayString: 'http://buff.ly/1LTbUqv',
+  url: 'https://austinstartups.com/what-is-a-product-designer-who-cares-eb38fc7afa7b#.i3r34a75x',
+  indices: [74, 96],
+}];
+
+const text = 'What is a Product Designer? An awesome story by @jgadapee over on Medium! http://buff.ly/1LTbUqv';
+
+const profile = {
+  name: 'Ash',
+  avatarUrl: 'https://buffer-uploads.s3.amazonaws.com/510521020a19000b6a00001e/a476fed03b1de4e06563d6063d7d3ee0.jpg',
+  email: 'ash@buffer.com',
+};
+
+const retweetProfile = {
+  name: 'Joel Gascoigne',
+  handle: '@joelgascoigne',
+  avatarUrl: 'https://buffer-uploads.s3.amazonaws.com/503a5c8ffc99f72a7f00002e/f49c2ff693f1c307af5e1b3d84e581ca.png',
+};
+
+
+storiesOf('TextPost')
+  .add('default', () => (
+    <TextPost
+      links={links}
+      profile={profile}
+      text={text}
+      onApproveClick={action('approve-click')}
+      onCancelConfirmClick={action('cancel-confirm-click')}
+      onDeleteClick={action('delete-click')}
+      onDeleteConfirmClick={action('delete-confirm-click')}
+      onEditClick={action('edit-click')}
+    />
+  ))
+  .add('retweet', () => (
+    <TextPost
+      links={links}
+      profile={profile}
+      text={text}
+      onApproveClick={action('approve-click')}
+      onCancelConfirmClick={action('cancel-confirm-click')}
+      onDeleteClick={action('delete-click')}
+      onDeleteConfirmClick={action('delete-confirm-click')}
+      onEditClick={action('edit-click')}
+      retweetProfile={retweetProfile}
+    />
+  ));
