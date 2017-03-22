d7b02b531e45deaa27613da6f0613450425b965f
Steve Dixon <stevemdixon@users.noreply.github.com>
2017-03-22 12:14:31 +0000

    Add MultipleImagesPost (#21)
    * Remove #fcfcfc background color
    
    This will default to white.
    
    * Set up MultipleImagesPost
    
    * Update to @bufferapp/components@0.4.1
    
    * Replace lochmara with curious-blue
    
    * Replace colors with previous versions
    
    This replaces uses of `lochmara` with `curious-blue` and `red` with
    `torchRed`.
    
    * Update MultipleImagesPost story
    
    This removes stories that are no longer required. We're adding a
    multipleImages prop to pass through an array of images. Properties of
    all objects in this file have been alphabetized.
    
    * Alphabetize all object properties
    
    in MultipleImagesPost/index.jsx
    
    * Add MultipleImagesPost component to storybook
    
    * Add imageUrls prop
    
    * Add default story
    
    * Remove unnecessary property
    
    * Rename torchRed to red, bump dependency number
    
    * Reinstate background-color
    
    * Update buffer-components dependency
    
    * Add border prop to MultipleImages & adjust height
    
    * Include MultipleImagesPost changes in snapshot
    
    * Update snapshot

---
+++ ColabTool/MultipleImagesPost/index.jsx
@@ -0,0 +1,82 @@
+import React, { PropTypes } from 'react';
+import {
+  LinkifiedText,
+  MultipleImages,
+} from '@bufferapp/components';
+import style from './style.css';
+import Post from '../Shared/Post';
+
+const MultipleImagesPost = ({
+  draftDetails,
+  imageUrls,
+  isConfirmingDelete,
+  isDeleting,
+  isWorking,
+  links,
+  manager,
+  onApproveClick,
+  onCancelConfirmClick,
+  onDeleteClick,
+  onDeleteConfirmClick,
+  onEditClick,
+  retweetProfile,
+  text,
+}) => {
+  const children = (
+    <div className={style['post-content']}>
+      <span className={style['post-content-text']}>
+        <LinkifiedText
+          links={links}
+        >
+          {text}
+        </LinkifiedText>
+      </span>
+      <div>
+        <MultipleImages
+          border={'rounded'}
+          height={'15rem'}
+          urls={imageUrls}
+          width={'20rem'}
+        />
+      </div>
+    </div>
+  );
+
+  return (
+    <Post
+      draftDetails={draftDetails}
+      isConfirmingDelete={isConfirmingDelete}
+      isDeleting={isDeleting}
+      isWorking={isWorking}
+      links={links}
+      manager={manager}
+      onApproveClick={onApproveClick}
+      onCancelConfirmClick={onCancelConfirmClick}
+      onDeleteClick={onDeleteClick}
+      onDeleteConfirmClick={onDeleteConfirmClick}
+      onEditClick={onEditClick}
+      retweetProfile={retweetProfile}
+      text={text}
+    >
+      {children}
+    </Post>
+  );
+};
+
+MultipleImagesPost.propTypes = {
+  ...Post.commonPropTypes,
+  imageUrls: PropTypes.arrayOf(PropTypes.string).isRequired,
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      displayString: PropTypes.string,
+      expandedUrl: PropTypes.string,
+      indices: PropTypes.arrayOf(PropTypes.number),
+      rawString: PropTypes.string,
+    }),
+  ).isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+MultipleImagesPost.defaultProps = Post.defaultProps;
+
+export default MultipleImagesPost;
---
+++ ColabTool/MultipleImagesPost/story.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  storiesOf,
+  action,
+} from '@kadira/storybook';
+import { checkA11y } from 'storybook-addon-a11y';
+import MultipleImagesPost from './index';
+
+const links = [{
+  displayString: 'http://buff.ly/1LTbUqv',
+  indices: [74, 96],
+  rawString: 'http://buff.ly/1LTbUqv',
+  url: 'https://austinstartups.com/what-is-a-product-designer-who-cares-eb38fc7afa7b#.i3r34a75x',
+}];
+
+const text = 'What is a Product Designer? An awesome story by @jgadapee over on Medium! http://buff.ly/1LTbUqv';
+
+const draftDetails = {
+  avatarUrl: 'https://buffer-uploads.s3.amazonaws.com/510521020a19000b6a00001e/a476fed03b1de4e06563d6063d7d3ee0.jpg',
+  createdAt: 'March 2nd at 12:45pm (GMT)',
+  email: 'ash@buffer.com',
+  postAction: 'This post is scheduled for 9:42pm (GMT)',
+  userName: 'Ash',
+  via: 'web',
+};
+
+const imageUrls = [
+  'http://lorempixel.com/400/400/cats/',
+  'http://lorempixel.com/400/400/cats/',
+  'http://lorempixel.com/400/400/cats/',
+  'http://lorempixel.com/400/400/cats/',
+];
+
+storiesOf('MultipleImagesPost')
+  .addDecorator(checkA11y)
+  .add('default', () => (
+    <MultipleImagesPost
+      draftDetails={draftDetails}
+      links={links}
+      imageUrls={imageUrls}
+      onApproveClick={action('approve-click')}
+      onCancelConfirmClick={action('cancel-confirm-click')}
+      onDeleteClick={action('delete-click')}
+      onDeleteConfirmClick={action('delete-confirm-click')}
+      onEditClick={action('edit-click')}
+      text={text}
+    />
+  ));
