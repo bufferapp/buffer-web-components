7d2d746fbdab44e0840c40f061275d6534450211
Melissa Alvarez <melissa@buffer.com>
2017-11-16 17:01:17 -0800

    Add pause config to schedule table

--- ScheduleSetting/ScheduleTable/ScheduleTableHeader/index.jsx
+++ ScheduleSetting/ScheduleTable/ScheduleTableHeader/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { borderWidth } from '@bufferapp/components/style/border';
 import { mystic } from '@bufferapp/components/style/color';
-import { Text } from '@bufferapp/components';
+import { Text, Button } from '@bufferapp/components';
 
 const headerStyle = {
   paddingTop: '1rem',
@@ -12,19 +12,26 @@ const headerStyle = {
 
 const ScheduleTableHeader = ({
   dayName,
+  paused,
+  onPauseToggleClick,
 }) => (
   <div style={headerStyle}>
     <Text
       color={'outerSpace'}
       size={'small'}
     >
-      {dayName}
+      {'Bob'}
     </Text>
+    <Button linkStyle onClick={() => onPauseToggleClick(dayName, paused)}>
+      {`Turn ${paused ? 'on' : 'off'}`}
+    </Button>
   </div>
 );
 
 ScheduleTableHeader.propTypes = {
   dayName: PropTypes.string.isRequired,
+  paused: PropTypes.bool.isRequired,
+  onPauseToggleClick: PropTypes.func.isRequired,
 };
 
 export default ScheduleTableHeader;
